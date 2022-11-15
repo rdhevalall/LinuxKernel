2524f68cc7af47435013375d4799d7481fe9f86c
rdhevalall <46484524+rdhevalall@users.noreply.github.com>
2022-11-15 15:54:21 +0200

    Update hello-1.c

--- hello-1.c
+++ hello-1.c
@@ -1,20 +1,17 @@
-/* 
- * hello-1.c - The simplest kernel module. 
- */ 
-#include <linux/kernel.h> /* Needed for pr_info() */ 
-#include <linux/module.h> /* Needed by all modules */ 
- 
-int init_module(void) 
-{ 
-    pr_info("Hello world 1.\n"); 
- 
-    /* A non 0 return means init_module failed; module can't be loaded. */ 
-    return 0; 
-} 
- 
-void cleanup_module(void) 
-{ 
-    pr_info("Goodbye world 1.\n"); 
-} 
- 
+#include<linux/module.h>    //needed for module_init and module_exit
+#include<linux/kernel.h>    //needed for KERN_INFO
+#include<linux/init.h>      //needed for the macros
+
+int __init hw_init(void) {
+        printk(KERN_INFO"Hello World\n");
+        return 0;
+}
+
+void __exit hw_exit(void) {
+        printk(KERN_INFO"Bye World\n");
+}
+
 MODULE_LICENSE("GPL");
+
+module_init(hw_init);
+module_exit(hw_exit);
