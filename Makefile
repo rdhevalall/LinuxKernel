a3ea531c87035881c8972a359a91dae226e1ec99
Dhevalall <rakeshh.dhevalall1@assaabloy.com>
2023-01-20 16:37:36 +0200

    rollback

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
-obj-m := hello.o
+obj-m += dvt-driver.o
+
 all:
-make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
+	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
 clean:
-make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
+    make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
--- hello-1.c
+++ hello-1.c
@@ -1,32 +1,18 @@
-#include<linux/module.h>    //needed for module_init and module_exit
-#include<linux/kernel.h>    //needed for KERN_INFO
-#include<linux/init.h>      //needed for the macros
-
-static struct proc_dir_entry* proc_entry;
-static ssize_t custom_read(struct file* file, char __user* user_buffer, size_t count, loff_t* offset)
-{
- printk(KERN_INFO "calling our very own custom read method.");
- char greeting[] = "Hello world!\n";
- int greeting_length = strlen(greeting);
- if (*offset > 0)
-  return 0;
- copy_to_user(user_buffer, greeting, greeting_length);
- *offset = greeting_length;
- return greeting_length;
-}
-static struct file_operations fops =
-{
- .owner = THIS_MODULE,
- .read = custom_read
-};
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/uaccess.h>
+#include <linux/fs.h>
+#include <linux/proc_fs.h>
+// Module metadata
+MODULE_AUTHOR("Ruan de Bruyn");
+MODULE_DESCRIPTION("Hello world driver");
+MODULE_LICENSE("GPL");
 // Custom init and exit methods
 static int __init custom_init(void) {
- proc_entry = proc_create("helloworlddriver", 0666, NULL, &fops);
  printk(KERN_INFO "Hello world driver loaded.");
  return 0;
 }
 static void __exit custom_exit(void) {
- proc_remove(proc_entry);
  printk(KERN_INFO "Goodbye my friend, I shall miss you dearly...");
 }
 module_init(custom_init);
